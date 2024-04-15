33bffb5690175a6e72cc1ec5f9094442ae68eae0
F1RE88 <139506660+F1RE88@users.noreply.github.com>
2024-04-15 22:17:34 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var s='now we have take 10$ only of your credit card and the t-shirt will come soon'
+var d='now we have take 10$ only of your credit card and the jeans will come soon'
+var f='F1RE88'
+function startbtn(){
+    alert(s)
+}
+function startbtna(){
+    alert(d)
+}
+function sd(){
+    alert(f)
+}
